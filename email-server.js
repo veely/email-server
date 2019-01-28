e54bb253e939606ec885295c947c455acb954394
veely1 <veely1@gmail.com>
2019-01-28 03:06:23 -0500

    cleaned up code. removed unused dependencies

--- email-server.js
+++ email-server.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const PORT = 3001;
@@ -22,7 +20,7 @@ const path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/send-email', (req, res) => {
-  const from = req.body.fromEmail,
+  let from = req.body.fromEmail,
   to = req.body.toEmail.split(','),
   subject = req.body.subject,
   body_html = req.body.body_html,
@@ -32,35 +30,37 @@ app.post('/send-email', (req, res) => {
     body_text = req.body.body_text;
   }
 
+  let emailParams = {
+    Destination: {
+      ToAddresses: to
+    }, 
+    Message: {
+      Body: {
+        Html: {
+        Charset: "UTF-8", 
+        Data: body_html
+        }, 
+        Text: {
+        Charset: "UTF-8", 
+        Data: body_text
+        }
+      }, 
+      Subject: {
+        Charset: "UTF-8", 
+        Data: subject
+      }
+    },
+    Source: from
+  };
+  
+  let email = new Email(emailParams);
+
   MongoClient.connect(mongodb_uri, (err, client) => {
     if (err) {
       console.error(`Failed to connect: ${mongodb_uri}`);
       throw err;
     }
     console.log(`Successfully connected: ${mongodb_uri}`);
-    let emailParams = {
-      Destination: {
-        ToAddresses: to
-      }, 
-      Message: {
-        Body: {
-          Html: {
-          Charset: "UTF-8", 
-          Data: body_html
-          }, 
-          Text: {
-          Charset: "UTF-8", 
-          Data: body_text
-          }
-        }, 
-        Subject: {
-          Charset: "UTF-8", 
-          Data: subject
-        }
-      },
-      Source: from
-    };
-    let email = new Email(emailParams);
     send(email, client, ses).then( result => {
       client.close();
       res.send(result);
